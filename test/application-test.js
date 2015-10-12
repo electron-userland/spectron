e37630a9ff5b8f7750b57457e74becfacd4d22d3
Kevin Sawicki <kevinsawicki@gmail.com>
2015-10-12 16:41:59 -0700

    Add assert for page text

--- test/application-test.js
+++ test/application-test.js
@@ -34,6 +34,6 @@ describe('application loading', function () {
       assert.equal(dimensions.y, 35)
       assert.equal(dimensions.width, 200)
       assert.equal(dimensions.height, 100)
-    }).then(done, done)
+    }).waitUntilContainsText('html', 'Hello').then(done, done)
   })
 })
