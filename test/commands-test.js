967bf45e161a0a1901f94b0c8ab0570ef11510e5
Kevin Sawicki <kevinsawicki@gmail.com>
2015-10-12 16:56:14 -0700

    Add setWindowDimensions command test

---
+++ test/commands-test.js
@@ -0,0 +1,39 @@
+var Application = require('../index').Application
+var assert = require('assert')
+var path = require('path')
+
+var describe = global.describe
+var it = global.it
+var beforeEach = global.beforeEach
+var afterEach = global.afterEach
+
+describe('window commands', function () {
+  this.timeout(10000)
+
+  var app = null
+
+  beforeEach(function (done) {
+    app = new Application({
+      path: path.join(__dirname, 'fixtures', 'app', 'app.js')
+    })
+    app.start(done)
+  })
+
+  afterEach(function (done) {
+    if (app) {
+      app.stop(done)
+    }
+    app = null
+  })
+
+  describe('setWindowDimensions', function () {
+    it('sets the window dimensions', function (done) {
+      app.client.setWindowDimensions(100, 200, 50, 75).getWindowDimensions().then(function (dimensions) {
+        assert.equal(dimensions.x, 100)
+        assert.equal(dimensions.y, 200)
+        assert.equal(dimensions.width, 50)
+        assert.equal(dimensions.height, 75)
+      }).then(done, done)
+    })
+  })
+})
