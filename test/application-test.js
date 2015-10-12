daa051475f38eebe7fc875f96017e79901af5877
Kevin Sawicki <kevinsawicki@gmail.com>
2015-10-12 15:37:28 -0700

    Add initial spec

---
+++ test/application-test.js
@@ -0,0 +1,32 @@
+var Application = require('../index').Application
+var assert = require('assert')
+var path = require('path')
+
+describe('application loading', function () {
+  this.timeout(10000)
+
+  var app = null
+
+  beforeEach(function (done) {
+    app = new Application({
+      path: path.join(__dirname, '..', 'node_modules', '.bin', 'electron'),
+      args: [
+        path.join(__dirname, 'fixtures', 'app')
+      ]
+    })
+    app.start(done)
+  })
+
+  afterEach(function (done) {
+    if (app) {
+      app.stop(done)
+    }
+    app = null
+  })
+
+  it('launches the application', function (done) {
+    app.client.windowHandles().then(function (response) {
+      assert.equal(response.value.length, 1)
+    }).then(done, done)
+  })
+})
---
+++ test/fixtures/app/main.js
@@ -0,0 +1,14 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+var mainWindow = null
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadUrl('file://' + __dirname + '/index.html')
+  mainWindow.on('closed', function() { mainWindow = null })
+})
+
+app.on('window-all-closed', function() {
+  app.quit()
+})
