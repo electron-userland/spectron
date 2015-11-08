0068c2f279ca654f6c192924d0e990b516a272f1
Kevin Sawicki <kevinsawicki@gmail.com>
2015-11-08 11:47:01 -0800

    Add cross-platform travis-build.sh script

---
+++ script/travis-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/creationix/nvm.git /tmp/.nvm
+source /tmp/.nvm/nvm.sh
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+fi
+
+npm install
+npm test
