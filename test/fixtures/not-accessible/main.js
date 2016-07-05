3cd726a6c3086ae306fde47715b602572ab44138
Kevin Sawicki <kevinsawicki@gmail.com>
2016-07-05 14:07:41 -0700

    Remove trailing comma

--- test/fixtures/not-accessible/main.js
+++ test/fixtures/not-accessible/main.js
@@ -7,7 +7,7 @@ app.on('ready', function () {
   mainWindow = new BrowserWindow({
     center: true,
     width: 800,
-    height: 600,
+    height: 600
   })
   mainWindow.loadURL('file://' + __dirname + '/index.html')
   mainWindow.on('closed', function () { mainWindow = null })
