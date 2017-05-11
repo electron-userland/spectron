8a010777c79de3b20791a3a0dddbb737e2ff9212
Kevin Sawicki <kevinsawicki@gmail.com>
2017-05-10 17:50:54 -0700

    Remove unused protocol

--- test/fixtures/slow/main.js
+++ test/fixtures/slow/main.js
@@ -1,20 +1,9 @@
 var app = require('electron').app
 var BrowserWindow = require('electron').BrowserWindow
-var protocol = require('electron').protocol
 
 var mainWindow = null
 
 app.on('ready', function () {
-  protocol.registerBufferProtocol('spectron', (request, callback) => {
-    if (request.url === 'spectron://test') {
-      callback({mimeType: 'text/html', data: new Buffer('<img src="spectron://img">')})
-    }
-  }, (error) => {
-    if (error != null) {
-      app.exit(1)
-    }
-  })
-
   mainWindow = new BrowserWindow({
     x: 25,
     y: 35,
