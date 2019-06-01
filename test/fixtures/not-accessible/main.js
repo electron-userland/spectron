553c810295fd1a4bf0dbac5a393e0d4fb1235f97
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2019-06-01 20:16:31 +0200

    Add webPreferences flag

--- test/fixtures/not-accessible/main.js
+++ test/fixtures/not-accessible/main.js
@@ -6,7 +6,10 @@ app.on('ready', function () {
   mainWindow = new BrowserWindow({
     center: true,
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   })
   mainWindow.loadFile('index.html')
   mainWindow.on('closed', function () { mainWindow = null })
