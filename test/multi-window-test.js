52c75db2e864ccc2dd717fd36f2edca7d566754a
goosewobbler <goosewobbler@pm.me>
2021-07-17 01:25:46 +0100

    replace switchwindow with windowByIndex

--- test/multi-window-test.js
+++ test/multi-window-test.js
@@ -25,16 +25,16 @@ describe('multiple windows', function () {
     return helpers.stopApplication(app);
   });
 
-  it('should switch focus using switchWindow', async function () {
+  it('should switch focus using windowByIndex', async function () {
     const windowCount = await app.client.getWindowCount();
     windowCount.should.equal(2);
-    await app.client.switchWindow('Top');
+    await app.client.windowByIndex(0);
     await app.client.getTitle().should.eventually.equal('Top');
-    await app.client.switchWindow('Bottom');
+    await app.client.windowByIndex(1);
     await app.client.getTitle().should.eventually.equal('Bottom');
-    await app.client.switchWindow('index-top.html');
+    await app.client.windowByIndex(0);
     await app.client.getTitle().should.eventually.equal('Top');
-    await app.client.switchWindow('index-bottom.html');
+    await app.client.windowByIndex(1);
     await app.client.getTitle().should.eventually.equal('Bottom');
   });
 });
