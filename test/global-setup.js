e22acb064c78edcae7aa699e2a1a3ce34d6ef9d6
goosewobbler <goosewobbler@pm.me>
2021-07-20 22:54:15 +0100

    more linting fixes

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,24 +1,23 @@
-const Application = require('..').Application;
+const path = require('path');
 const assert = require('assert');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const chaiRoughly = require('chai-roughly');
+const { Application } = require('..');
 
-const path = require('path');
-
-global.before(function () {
+global.before(() => {
   chai.should();
   chai.use(chaiAsPromised);
   chai.use(chaiRoughly);
 });
 
-exports.getElectronPath = function () {
+exports.getElectronPath = () => {
   let electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
   if (process.platform === 'win32') electronPath += '.cmd';
   return electronPath;
 };
 
-exports.setupTimeout = function (test) {
+exports.setupTimeout = (test) => {
   if (process.env.CI) {
     test.timeout(30000);
   } else {
@@ -26,20 +25,24 @@ exports.setupTimeout = function (test) {
   }
 };
 
-exports.startApplication = function (options) {
-  options.path = exports.getElectronPath();
-  if (process.env.CI) options.startTimeout = 30000;
+exports.startApplication = async (options) => {
+  const appOpts = { ...options, path: exports.getElectronPath() };
+  if (process.env.CI) {
+    appOpts.startTimeout = 30000;
+  }
 
-  const app = new Application(options);
-  return app.start().then(function () {
-    assert.strictEqual(app.isRunning(), true);
-    chaiAsPromised.transferPromiseness = app.transferPromiseness;
-    return app;
-  });
+  const app = new Application(appOpts);
+  await app.start();
+
+  assert.strictEqual(app.isRunning(), true);
+  chaiAsPromised.transferPromiseness = app.transferPromiseness;
+  return app;
 };
 
-exports.stopApplication = async function (app) {
-  if (!app || !app.isRunning()) return;
+exports.stopApplication = async (app) => {
+  if (!app || !app.isRunning()) {
+    return;
+  }
 
   await app.stop();
   assert.strictEqual(app.isRunning(), false);
