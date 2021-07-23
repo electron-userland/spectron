c2a09fb306a921b858f5c030b6b52dff5ae5b3f1
goosewobbler <goosewobbler@pm.me>
2021-07-23 17:11:44 +0100

    add getAppPath

--- test/e2e/application.spec.ts
+++ test/e2e/application.spec.ts
@@ -3,9 +3,29 @@ import { setupBrowser, WebdriverIOBoundFunctions } from '@testing-library/webdri
 import { queries } from '@testing-library/dom';
 import { Application } from '@goosewobbler/spectron';
 
-describe('application loading', function () {
+function getAppPath(distPath: string, appName: string) {
+  enum supportedPlatform {
+    darwin = 'darwin',
+    linux = 'linux',
+    win32 = 'win32',
+  }
+  const supportedPlatforms = ['darwin', 'linux', 'win32'];
+  if (!supportedPlatforms.includes(process.platform)) {
+    throw new Error('unsupported platform');
+  }
+  const pathMap = {
+    darwin: `mac/${appName}.app/Contents/MacOS/${appName}`,
+    linux: `linux-unpacked/${appName}`,
+    win32: `mac/${appName}.app/Contents/MacOS/${appName}`,
+  };
+
+  return `${distPath}/${pathMap[process.platform as supportedPlatform]}`;
+}
+
+describe('application loading', () => {
+  const appPath = getAppPath(path.join(process.cwd(), 'dist'), 'test');
   const app = new Application({
-    path: path.join(process.cwd(), 'dist/mac/test.app/Contents/MacOS/test'),
+    path: appPath,
     chromeDriverLogPath: path.join(process.cwd(), 'chromeDriver.log'),
   });
 
@@ -25,7 +45,7 @@ describe('application loading', function () {
       }
     }, 30000);
 
-    it('launches the application', async function () {
+    it('launches the application', async () => {
       const response = await app.client.getWindowHandles();
       expect(response.length).toEqual(1);
 
