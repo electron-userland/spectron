9eaa07bb4c33a155ab14bd9003bd64f03a44e39c
goosewobbler <goosewobbler@pm.me>
2021-07-24 15:02:27 +0100

    add a couple of tests

--- test/unit/api.spec.ts
+++ test/unit/api.spec.ts
@@ -1,30 +1,63 @@
-import { createApi } from '@goosewobbler/spectron/lib/api';
+import { anyFunction, mock, MockProxy } from 'jest-mock-extended';
+import { createApi, SpectronClient } from '@goosewobbler/spectron/lib/api';
 
-const mockWebDriverClient: {
-  executeAsync?: jest.Mock;
-  addCommand?: jest.Mock;
-} = {};
-let loadApiFunc;
-let apiNamesArr;
+let mockWebDriverClient: MockProxy<SpectronClient>;
 let mockApiPlaceholders: {};
 
 beforeEach(() => {
   mockApiPlaceholders = {
-    mockNS1: {
-      mockFn1: `spectron.mockNS1.mockFn1`,
-      mockFn2: `spectron.mockNS1.mockFn2`,
-      mockFn3: `spectron.mockNS1.mockFn3`,
+    mockApi1: {
+      mockFn1: `spectron.mockApi1.mockFn1`,
+      mockFn2: `spectron.mockApi1.mockFn2`,
+      mockFn3: `spectron.mockApi1.mockFn3`,
+    },
+    mockApi2: {
+      mockFn1: `spectron.mockApi2.mockFn1`,
+      mockFn2: `spectron.mockApi2.mockFn2`,
+      mockFn3: `spectron.mockApi2.mockFn3`,
+    },
+    mockApi3: {
+      mockFn1: `spectron.mockApi3.mockFn1`,
+      mockFn2: `spectron.mockApi3.mockFn2`,
+      mockFn3: `spectron.mockApi3.mockFn3`,
     },
   };
-  mockWebDriverClient.executeAsync = jest.fn().mockImplementationOnce((loadApi, apiNames) => {
-    loadApiFunc = loadApi;
-    apiNamesArr = apiNames;
-    return Promise.resolve(mockApiPlaceholders);
-  });
-  mockWebDriverClient.addCommand = jest.fn();
+  mockWebDriverClient = mock<SpectronClient>();
+  mockWebDriverClient.executeAsync.mockImplementationOnce(() => Promise.resolve(mockApiPlaceholders));
 });
 
-it('should pass', async () => {
+it('should return the expected interface', async () => {
   const api = await createApi(mockWebDriverClient, ['mockApi1', 'mockApi2', 'mockApi3']);
-  expect(typeof createApi === 'function');
+  expect(api).toEqual({
+    mockApi1: {
+      mockFn1: anyFunction(),
+      mockFn2: anyFunction(),
+      mockFn3: anyFunction(),
+    },
+    mockApi2: {
+      mockFn1: anyFunction(),
+      mockFn2: anyFunction(),
+      mockFn3: anyFunction(),
+    },
+    mockApi3: {
+      mockFn1: anyFunction(),
+      mockFn2: anyFunction(),
+      mockFn3: anyFunction(),
+    },
+  });
+});
+
+it('should add the expected client commands', async () => {
+  await createApi(mockWebDriverClient, ['mockApi1', 'mockApi2', 'mockApi3']);
+  expect(mockWebDriverClient.addCommand.mock.calls).toEqual([
+    ['spectron.mockApi1.mockFn1', anyFunction()],
+    ['spectron.mockApi1.mockFn2', anyFunction()],
+    ['spectron.mockApi1.mockFn3', anyFunction()],
+    ['spectron.mockApi2.mockFn1', anyFunction()],
+    ['spectron.mockApi2.mockFn2', anyFunction()],
+    ['spectron.mockApi2.mockFn3', anyFunction()],
+    ['spectron.mockApi3.mockFn1', anyFunction()],
+    ['spectron.mockApi3.mockFn2', anyFunction()],
+    ['spectron.mockApi3.mockFn3', anyFunction()],
+  ]);
 });
