8440b1aa7f11ca3d8c6a4545012a0b1e63126309
goosewobbler <goosewobbler@pm.me>
2021-07-24 04:17:48 +0100

    start of test

--- test/unit/api.spec.ts
+++ test/unit/api.spec.ts
@@ -1,5 +1,30 @@
 import { createApi } from '@goosewobbler/spectron/lib/api';
 
-it('should pass', () => {
+const mockWebDriverClient: {
+  executeAsync?: jest.Mock;
+  addCommand?: jest.Mock;
+} = {};
+let loadApiFunc;
+let apiNamesArr;
+let mockApiPlaceholders: {};
+
+beforeEach(() => {
+  mockApiPlaceholders = {
+    mockNS1: {
+      mockFn1: `spectron.mockNS1.mockFn1`,
+      mockFn2: `spectron.mockNS1.mockFn2`,
+      mockFn3: `spectron.mockNS1.mockFn3`,
+    },
+  };
+  mockWebDriverClient.executeAsync = jest.fn().mockImplementationOnce((loadApi, apiNames) => {
+    loadApiFunc = loadApi;
+    apiNamesArr = apiNames;
+    return Promise.resolve(mockApiPlaceholders);
+  });
+  mockWebDriverClient.addCommand = jest.fn();
+});
+
+it('should pass', async () => {
+  const api = await createApi(mockWebDriverClient, ['mockApi1', 'mockApi2', 'mockApi3']);
   expect(typeof createApi === 'function');
 });
