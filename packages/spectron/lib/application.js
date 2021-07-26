148b7f3fef613d213346b16db64f45f464d0f8d9
goosewobbler <goosewobbler@pm.me>
2021-07-26 16:28:34 +0100

    give up for now

--- packages/spectron/lib/application.js
+++ packages/spectron/lib/application.js
@@ -155,16 +155,16 @@ Application.prototype.createClient = async function createClient() {
   const isWin = process.platform === 'win32';
   const launcherPath = path.join(__dirname, isWin ? 'launcher.bat' : 'launcher.js');
 
-  // if (process.env.CI) {
-  args.push('--headless');
-  args.push('--no-sandbox');
-  args.push('--disable-dev-shm-usage');
-  args.push('blink-settings=imagesEnabled=false');
-  args.push('--disable-gpu');
-  // args.push('--remote-debugging-port=9222');
-  args.push('--disable-infobars');
-  args.push('--disable-extensions');
-  // }
+  if (process.env.CI) {
+    args.push('--headless');
+    args.push('--no-sandbox');
+    args.push('--disable-dev-shm-usage');
+    args.push('blink-settings=imagesEnabled=false');
+    args.push('--disable-gpu');
+    // args.push('--remote-debugging-port=9222');
+    args.push('--disable-infobars');
+    args.push('--disable-extensions');
+  }
 
   const options = {
     hostname: self.host,
@@ -197,7 +197,7 @@ Application.prototype.createClient = async function createClient() {
   } catch (error) {
     const data = await fs.readFile('/home/runner/work/spectron/spectron/test/chromeDriver.log', 'utf8');
     console.log(data);
-    throw new Error(`Webdriver error yo: ${error.message} ${data}`);
+    throw new Error(`Webdriver error: ${error.message} ${data}`);
   }
 };
 
