9d38baf1dded89d8aee3b2812e38063df22897d6
goosewobbler <goosewobbler@pm.me>
2021-07-30 02:31:49 +0100

    update types

--- packages/spectron/lib/spectron.d.ts
+++ packages/spectron/lib/spectron.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for spectron v3.6.0
-// Project: https://github.com/electron/spectron
-// Definitions by: deerares <https://github.com/deerares>
+// Type definitions for spectron v17.0.0
+// Project: https://github.com/goosewobbler/spectron
+// Definitions by: goosewobbler <https://github.com/goosewobbler>
 
 /// <reference types="node" />
 
@@ -45,16 +45,6 @@ declare module '@goosewobbler/spectron' {
      * Get the selected text in the current window.
      */
     getSelectedText(): Promise<string>;
-    /**
-     * Gets the console log output from the render process.
-     * The logs are cleared after they are returned.
-     */
-    getRenderProcessLogs(): Promise<object[]>;
-    /**
-     * Gets the console log output from the main process.
-     * The logs are cleared after they are returned.
-     */
-    getMainProcessLogs(): Promise<string[]>;
   }
 
   export interface SpectronWindow extends Electron.BrowserWindow {}
@@ -68,126 +58,14 @@ declare module '@goosewobbler/spectron' {
   export interface SpectronRendererProcess extends NodeJS.Process {}
 
   type BasicAppSettings = {
-    /**
-     * String host name of the launched chromedriver process. Defaults to 'localhost'.
-     */
-    host?: string;
-    /**
-     *  Number port of the launched chromedriver process. Defaults to 9515.
-     */
-    port?: number;
-    /**
-     * Number in milliseconds to wait for application quitting.
-     * Defaults to 1000 milliseconds.
-     */
     quitTimeout?: number;
-    /**
-     * Number in milliseconds to wait for ChromeDriver to start.
-     * Defaults to 5000 milliseconds.
-     */
-    startTimeout?: number;
-    /**
-     *  Number in milliseconds to wait for calls like waitUntilTextExists and
-     *  waitUntilWindowLoaded to complete.
-     *  Defaults to 5000 milliseconds.
-     */
-    waitTimeout?: number;
-    /**
-     * Number of retry attempts to make when connecting to ChromeDriver.
-     * Defaults to 10 attempts.
-     */
-    connectionRetryCount?: number;
-    /**
-     * Number in milliseconds to wait for connections to ChromeDriver to be made.
-     * Defaults to 30000 milliseconds.
-     */
-    connectionRetryTimeout?: number;
-    /**
-     * String path to a node executable to launch ChromeDriver with.
-     * Defaults to process.execPath.
-     */
-    nodePath?: string;
-    /** String path to the Electron application executable to launch.
-     * Note: If you want to invoke electron directly with your app's main script then you should
-     * specify path as electron via electron-prebuilt and specify your app's main script path as
-     * the first argument in the args array.
-     */
-    path: string;
-    /**
-     * Array of arguments to pass to the Electron application.
-     */
-    args?: string[];
-    /**
-     * Array of arguments to pass to ChromeDriver.
-     * See here (https://sites.google.com/a/chromium.org/chromedriver/capabilities) for details
-     * on the Chrome arguments.
-     */
-    chromeDriverArgs?: string[];
-    /**
-     * Object of additional environment variables to set in the launched application.
-     */
-    env?: object;
-    /**
-     *  String address of a Chrome debugger server to connect to.
-     */
-    debuggerAddress?: string;
-    /**
-     * String path to file to store ChromeDriver logs in.
-     * Setting this option enables --verbose logging when starting ChromeDriver.
-     */
-    chromeDriverLogPath?: string;
-    /**
-     * String path to a directory where Webdriver will write logs to.
-     * Setting this option enables verbose logging from Webdriver.
-     */
-    webdriverLogPath?: string;
-    /**
-     * Extra Webdriver options
-     */
-    webdriverOptions?: object;
-  };
-  type AppConstructorOptions = BasicAppSettings & {
-    /**
-     *  String path to the working directory to use for the launched application.
-     *  Defaults to process.cwd().
-     */
-    cwd?: string;
-  };
-  export type ApplicationSettings = BasicAppSettings & {
-    /**
-     *  String path to the working directory to use for the launched application.
-     *  Defaults to process.cwd().
-     */
-    workingDirectory?: string;
   };
 
-  /**
-   * Start and stop your Electron application.
-   */
-  export class Application {
-    /**
-     * Spectron uses WebdriverIO and exposes the managed client property on the created
-     * Application instances.
-     * The full client API provided by WebdriverIO can be found here
-     * http://webdriver.io/api.html
-     * Several additional commands are provided specific to Electron.
-     */
+  export interface SpectronApp {
     client: SpectronClient;
 
-    /**
-     * The browserWindow property is an alias for require('electron').remote.getCurrentWindow().
-     * It provides you access to the current BrowserWindow and contains all the APIs.
-     * https://electron.atom.io/docs/api/browser-window/
-     */
     browserWindow: SpectronWindow;
 
-    /**
-     * The webContents property is an alias for
-     * require('electron').remote.getCurrentWebContents().
-     * It provides you access to the WebContents for the current window
-     * and contains all the APIs.
-     * https://electron.atom.io/docs/api/web-contents/
-     */
     webContents: SpectronWebContents;
 
     app: SpectronElectronApp;
@@ -198,36 +76,9 @@ declare module '@goosewobbler/spectron' {
 
     rendererProcess: SpectronRendererProcess;
 
-    constructor(options: AppConstructorOptions);
-
-    /**
-     * Starts the application.
-     * Returns a Promise that will be resolved when the application is ready to use.
-     * You should always wait for start to complete before running any commands.
-     */
-    start(): Promise<Application>;
-
-    /**
-     * Stops the application.
-     * Returns a Promise that will be resolved once the application has stopped.
-     */
-    stop(): Promise<Application>;
-
-    /**
-     * Stops the application and then starts it.
-     * Returns a Promise that will be resolved once the application has started again.
-     */
-    restart(): Promise<Application>;
-
-    /**
-     * Checks to determine if the application is running or not.
-     */
-    isRunning(): boolean;
-
-    /**
-     * Get all the configured options passed to the new Application() constructor.
-     * This will include the default options values currently being used.
-     */
-    getSettings(): ApplicationSettings;
+    quit(): Promise<void>;
   }
+
+  export function initSpectron(appSettings: BasicAppSettings): Promise<SpectronApp>;
+  export function run(): Promise<void>;
 }
