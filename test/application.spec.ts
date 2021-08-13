8e087525076bb73827112505b5a383bdc52f02fa
goosewobbler <goosewobbler@pm.me>
2021-08-13 19:56:37 +0100

    remove console logs

--- test/application.spec.ts
+++ test/application.spec.ts
@@ -14,7 +14,6 @@ describe('application loading', () => {
 
   describe('App', () => {
     it('should launch the application', async () => {
-      console.log('start of first test');
       const response = await app.client.getWindowHandles();
       expect(response.length).toEqual(1);
 
@@ -24,7 +23,6 @@ describe('application loading', () => {
       await app.client.waitUntilTextExists('html', 'Hello');
       const title = await app.client.getTitle();
       expect(title).toEqual('Test');
-      console.log('end of first test');
     });
   });
 });
