6c71ab0cca3da04d48db39859f7142b92ee93be6
goosewobbler <goosewobbler@pm.me>
2021-09-13 17:44:43 +0100

    TSify

--- packages/wdio-spectron-service/src/launcher.ts
+++ packages/wdio-spectron-service/src/launcher.ts
@@ -1,10 +1,12 @@
-import { spawn } from 'child_process';
+/* eslint no-underscore-dangle: off */
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 import fs from 'fs-extra';
 import path from 'path';
 import split2 from 'split2';
 import { path as chromedriverPath } from 'chromedriver';
 import logger from '@wdio/logger';
-import tcpPortUsed from 'tcp-port-used';
+import { waitUntilUsed } from 'tcp-port-used';
+import EventEmitter from 'events';
 import getFilePath from './getFilePath';
 
 const log = logger('chromedriver');
@@ -19,29 +21,70 @@ const DEFAULT_CONNECTION = {
   path: '/',
 };
 
-const isMultiremote = (obj) => typeof obj === 'object' && !Array.isArray(obj);
-const isChrome = (cap) => cap.browserName && cap.browserName.toLowerCase() === 'chrome';
+type ChromeOptions = {
+  binary: string;
+  args: string[];
+  windowTypes: ['app', 'webview'];
+};
+
+type Options = {
+  'port': number;
+  'path': string;
+  'protocol': string;
+  'hostname': string;
+  'outputDir': string;
+  'logFileName': string;
+  'chromedriverCustomPath': string;
+  'args': string[];
+  'browserName'?: 'chrome';
+  'goog:chromeOptions'?: ChromeOptions;
+};
+
+type Capabilities = [
+  {
+    'browserName': 'chrome';
+    'goog:chromeOptions': ChromeOptions;
+  },
+];
+
+type Config = {
+  outputDir: string;
+};
 
 export default class ChromeDriverLauncher {
-  constructor(options, capabilities, config) {
+  constructor(options: Options, capabilities: Capabilities, config: Config) {
     this.options = {
-      protocol: options.protocol || DEFAULT_CONNECTION.protocol,
-      hostname: options.hostname || DEFAULT_CONNECTION.hostname,
-      port: options.port || DEFAULT_CONNECTION.port,
-      path: options.path || DEFAULT_CONNECTION.path,
-    };
+      'protocol': options.protocol || DEFAULT_CONNECTION.protocol,
+      'hostname': options.hostname || DEFAULT_CONNECTION.hostname,
+      'port': options.port || DEFAULT_CONNECTION.port,
+      'path': options.path || DEFAULT_CONNECTION.path,
+      'browserName': 'chrome',
+      'goog:chromeOptions': capabilities[0]['goog:chromeOptions'],
+    } as Options;
 
     this.outputDir = options.outputDir || config.outputDir;
     this.logFileName = options.logFileName || DEFAULT_LOG_FILENAME;
-    this.capabilities = capabilities;
     this.args = options.args || [];
     this.chromedriverCustomPath = options.chromedriverCustomPath
       ? path.resolve(options.chromedriverCustomPath)
-      : chromedriverPath;
+      : (chromedriverPath as string);
   }
 
-  async onPrepare() {
-    this.args.forEach((argument) => {
+  private options;
+
+  private outputDir;
+
+  private logFileName;
+
+  private args;
+
+  private chromedriverCustomPath;
+
+  private process?: ChildProcessWithoutNullStreams;
+
+  async onPrepare(): Promise<void> {
+    const args = this.args as string[];
+    args.forEach((argument: string) => {
       if (argument.includes('--port')) {
         throw new Error('Argument "--port" already exists');
       }
@@ -50,67 +93,49 @@ export default class ChromeDriverLauncher {
       }
     });
 
-    this.args.push(`--port=${this.options.port}`);
-    this.args.push(`--url-base=${this.options.path}`);
-
-    /**
-     * update capability connection options to connect
-     * to chromedriver
-     */
-    this._mapCapabilities();
+    args.push(`--port=${this.options.port}`);
+    args.push(`--url-base=${this.options.path}`);
 
     let command = this.chromedriverCustomPath;
-    log.info(`Start Chromedriver (${command}) with args ${this.args.join(' ')}`);
+    log.info(`Start Chromedriver (${command}) with args ${args.join(' ')}`);
     if (!fs.existsSync(command)) {
       log.warn('Could not find chromedriver in default path: ', command);
       log.warn('Falling back to use global chromedriver bin');
       command = process && process.platform === 'win32' ? 'chromedriver.exe' : 'chromedriver';
     }
-    this.process = spawn(command, this.args);
+    this.process = spawn(command, args);
 
     if (typeof this.outputDir === 'string') {
       this._redirectLogStream();
-    } else {
-      this.process.stdout.pipe(split2()).on('data', log.info);
-      this.process.stderr.pipe(split2()).on('data', log.warn);
+    } else if (this.process?.stdout && this.process?.stderr) {
+      const split = split2();
+      (this.process.stdout.pipe(split) as EventEmitter).on('data', (...logArgs) => log.info(...logArgs));
+      (this.process.stderr.pipe(split) as EventEmitter).on('data', (...logArgs) => log.warn(...logArgs));
     }
 
-    await tcpPortUsed.waitUntilUsed(this.options.port, POLL_INTERVAL, POLL_TIMEOUT);
+    await waitUntilUsed(this.options.port, POLL_INTERVAL, POLL_TIMEOUT);
     process.on('exit', this.onComplete.bind(this));
     process.on('SIGINT', this.onComplete.bind(this));
     process.on('uncaughtException', this.onComplete.bind(this));
   }
 
-  onComplete() {
+  onComplete(): void {
     if (this.process) {
       this.process.kill();
     }
   }
 
-  _redirectLogStream() {
+  _redirectLogStream(): void {
     const logFile = getFilePath(this.outputDir, this.logFileName);
 
     // ensure file & directory exists
     fs.ensureFileSync(logFile);
 
     const logStream = fs.createWriteStream(logFile, { flags: 'w' });
-    this.process.stdout.pipe(logStream);
-    this.process.stderr.pipe(logStream);
-  }
 
-  _mapCapabilities() {
-    if (isMultiremote(this.capabilities)) {
-      for (const cap in this.capabilities) {
-        if (isChrome(this.capabilities[cap].capabilities)) {
-          Object.assign(this.capabilities[cap], this.options);
-        }
-      }
-    } else {
-      for (const cap of this.capabilities) {
-        if (isChrome(cap)) {
-          Object.assign(cap, this.options);
-        }
-      }
+    if (this.process) {
+      this.process.stdout.pipe(logStream);
+      this.process.stderr.pipe(logStream);
     }
   }
 }
