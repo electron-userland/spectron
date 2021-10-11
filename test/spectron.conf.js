17f75753a546de68e2b9966a01eded4e4383675f
goosewobbler <goosewobbler@pm.me>
2021-10-11 20:47:48 +0100

    new config

--- test/spectron.conf.js
+++ test/spectron.conf.js
@@ -9,8 +9,40 @@ const {
 } = packageJson;
 
 const config = {
-  appPath: join(process.cwd(), 'app', 'dist'),
-  appName: productName,
+  spectronOpts: {
+    appPath: join(process.cwd(), 'app', 'dist'),
+    appName: productName,
+  },
+  // hostname: '127.0.0.1',
+  port: 9515,
+  waitforTimeout: 5000,
+  connectionRetryCount: 10,
+  connectionRetryTimeout: 30000,
+  logLevel: 'debug',
+  runner: 'local',
+  outputDir: 'all-logs',
+  specs: ['./*.spec.ts'],
+  autoCompileOpts: {
+    autoCompile: true,
+    // see https://github.com/TypeStrong/ts-node#cli-and-programmatic-options
+    // for all available options
+    tsNodeOpts: {
+      transpileOnly: true,
+      files: true,
+      moduleTypes: {
+        // WDIO doesn't currently support ESM
+        '*.conf.ts': 'cjs',
+        '*.spec.ts': 'cjs',
+      },
+      project: './tsconfig.json',
+    },
+    // tsconfig-paths is only used if "tsConfigPathsOpts" are provided, if you
+    // do please make sure "tsconfig-paths" is installed as dependency
+    tsConfigPathsOpts: {
+      baseUrl: './',
+    },
+  },
+  framework: 'mocha',
 };
 
 module.exports = { config };
