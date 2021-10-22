2c3e9d2c558ea2934c2e4ca88eb7f431595d90d2
goosewobbler <goosewobbler@pm.me>
2021-10-22 17:55:27 +0100

    add make smaller test

--- test/interaction.spec.ts
+++ test/interaction.spec.ts
@@ -31,29 +31,17 @@ describe('application loading', () => {
     });
   });
 
-  //   describe('when the make smaller button is clicked', function () {
-  //     it('decreases the window height and width by 10 pixels', async function () {
-  //       await app.client.waitUntilWindowLoaded();
-  //       await app.browserWindow.getBounds().should.eventually.have.property('width', 800);
-  //       await app.browserWindow.getBounds().should.eventually.have.property('height', 400);
-  //       const elem = await app.client.$('.btn-make-smaller');
-  //       await elem.click();
-  //       const bounds = await app.browserWindow.getBounds();
-  //       bounds.should.have.property('width', 790);
-  //       bounds.should.have.property('height', 390);
-  //     });
-  //   });
-
-  //   describe('getWindowCount', function () {
-  //     it('retrieves the window count', function () {
-  //       return app.client.getWindowCount().should.eventually.equal(1);
-  //     });
-  //   });
-
-  //   describe('waitUntilWindowLoaded()', function () {
-  //     it('waits until the current window is loaded', async function () {
-  //       await app.client.waitUntilWindowLoaded();
-  //       return app.webContents.isLoading().should.eventually.be.false;
-  //     });
-  //   });
+  describe('when the make smaller button is clicked', function () {
+    it('decreases the window height and width by 10 pixels', async function () {
+      await app.client.waitUntilWindowLoaded();
+      let bounds = (await app.browserWindow.getBounds()) as { width: number; height: number };
+      expect(bounds.width).toEqual(200);
+      expect(bounds.height).toEqual(300);
+      const elem = await app.client.$('.make-smaller');
+      await elem.click();
+      bounds = (await app.browserWindow.getBounds()) as { width: number; height: number };
+      expect(bounds.width).toEqual(190);
+      expect(bounds.height).toEqual(290);
+    });
+  });
 });
